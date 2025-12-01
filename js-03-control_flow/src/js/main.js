2f6297218ae70b42c996b3f3051b0abaa32b5de1
Sergio Torres <sergio.torres@idr.edu.mx>
2025-12-01 11:17:08 -0600

    feat: ejercicio de rango con switch case

--- js-03-control_flow/src/js/main.js
+++ js-03-control_flow/src/js/main.js
@@ -251,3 +251,96 @@ console.log( getAccessLevel("customer") ); // Acceso denegado
  Refactorizar la función getWeather usando switch-case
 */
 //const getWeatherSwitch=(codigo)=>
+    const getWeatherSwitch=(codigo)=>{
+     let message;
+     switch ( codigo){
+        case 0 :
+            message="Clear Sky";
+            break
+        case 1:
+        case 2:
+        case 3:
+            message="Mainly clear, partly cloudy, and overcast";
+            break;
+        case 45:
+        case 48: 
+            message="Fog and depositing rime fog";
+            break
+        default :
+            message="no definido";
+     }
+     return message;  
+    } 
+
+console.log (getWeatherSwitch(45));
+
+    const getWeatherSwitch2=(codigo)=>{
+     switch ( codigo){
+        case 0 :
+            return "Clear Sky";
+        case 1:
+        case 2:
+        case 3:
+            return "Mainly clear, partly cloudy, and overcast";
+        case 45:
+        case 48: 
+            return "Fog and depositing rime fog";
+        default :
+            return "no definido";
+     } 
+    }
+console.log (getWeatherSwitch2(0));
+
+// ==========================================================
+
+/*
+ Dado un número entero, imprimir:
+
+ "Negativo":  si el número es menor a 0.
+ "Cero":  si el número es  0.
+ "Positivo":  si el número es mayor a cero pero menor a 100.
+ "Grande": si el número es mayor o igual a 100.
+ */
+function clasificarNumeroIfElse(numero) {
+    if (numero < 0) {
+      return "Negativo";
+    } else if (numero === 0) {
+      return "Cero";
+    } else if (numero > 0 && numero < 100) {
+      return "Positivo";
+    } else {
+      return "Grande";
+    }
+  }
+  
+  console.log(clasificarNumeroIfElse(-5));   // Negativo
+  console.log(clasificarNumeroIfElse(0));    // Cero
+  console.log(clasificarNumeroIfElse(50));   // Positivo
+  console.log(clasificarNumeroIfElse(100));  // Grande
+
+  // ----- Function Switch ----------
+function evaluarElNumeroSwitch(numero) {
+
+    switch ( true ) {
+        case (numero < 0):
+            mensaje = "Negativo";
+            break;
+        case (numero === 0):
+            mensaje = "Cero";
+            break;
+        case (numero >0 && numero < 100):
+            mensaje = "Positivo";
+            break;
+        case (numero >= 100):
+            mensaje = "Grande";
+            break;
+        default:
+            mensaje = "Numero desconocido";
+    }
+    return mensaje;
+};
+
+console.log(evaluarElNumeroSwitch(-45) ); // Negativo
+console.log(evaluarElNumeroSwitch(0) ); // Cero
+console.log(evaluarElNumeroSwitch(45) ); // Positivo
+console.log(evaluarElNumeroSwitch(100) ); // Grande
