cca23b9c652b6bc271eabad477500c29111584cc
Genaro Paoli <mgenasp2403@gmail.com>
2025-12-03 12:56:08 -0600

    Añado script de main

--- js-05-oop/src/js/main.js
+++ js-05-oop/src/js/main.js
@@ -15,3 +15,18 @@ import { changePrincipalTitle } from "./modules/ChangePrincipalTittles";
 // crear una funcion que se le pasen el nombre de la imagen a mostrar (cat o dino)
 // Esta imagen es la que se debe de mostrar en el elemento con id "logo"
 
+// importanción nombrada
+import { changeLogo } from "./modules/changeLogo.js";
+import { changePrincipalTitle } from "./modules/changePrincipalTitle.js";
+import { aliceUser } from "./modules/literalObject.js";
+
+
+changePrincipalTitle("title");
+
+// Crear una función que se le pase el nombre de la imagen a mostrar (cat o dino)
+// esta imagen es la que se debe mostrar en el elemento con id "logo"
+changeLogo("cat.jpg");
+
+console.log( aliceUser.getFullName() );
+console.log( aliceUser );
+console.log( aliceUser.stringify() );
